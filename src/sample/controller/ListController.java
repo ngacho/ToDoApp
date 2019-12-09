19224c0a7b7c5605f178fe7c4817bb6a21138ad4
Ngacho <52356809+ngacho@users.noreply.github.com>
2019-12-09 05:24:45 +0300

    populate each list row with a customized list cell

--- src/sample/controller/ListController.java
+++ src/sample/controller/ListController.java
@@ -4,17 +4,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.*;
-import javafx.scene.control.cell.CheckBoxListCell;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Priority;
 import sample.animations.Fader;
+import sample.models.Task;
 
 import java.net.URL;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ResourceBundle;
 
 public class ListController {
@@ -28,36 +26,30 @@ public class ListController {
     @FXML
     private AnchorPane rootanchor_inList;
 
-
     @FXML
     private URL location;
 
     @FXML
     private Button addtaskbutton_inlist;
 
-
-
     @FXML
-    private ListView<String> taskListViw_inlist;
+    private ListView<Task> taskListViw_inlist;
 
-    ObservableList<String> listView = FXCollections.observableArrayList("John",
-            "Paulo",
-            "Mancity",
-            "Man United"
-    );
+    private ObservableList<Task> tasks;
 
-    CheckBoxListCell<String> checkBoxListCellList = new CheckBoxListCell<>();
-
-    public void setCheckBoxListCellList(CheckBoxListCell<String> checkBoxListCellList) {
-        this.checkBoxListCellList = checkBoxListCellList;
-        //TODO: understand and populate this thing
-    }
 
     @FXML
     void initialize() {
-        taskListViw_inlist.getItems().setAll(listView);
-        taskListViw_inlist.setCellFactory(param -> new Cell());
         addtaskbutton_inlist.setOnAction(actionEvent -> openAddTaskForm());
+
+        Task testTask = new Task(Timestamp.valueOf(LocalDateTime.now()), "Test Task", "We are testing to see if task is working");
+
+        tasks = FXCollections.observableArrayList();
+        tasks.add(testTask);
+
+        taskListViw_inlist.setItems(tasks);
+        taskListViw_inlist.setCellFactory(ListRowController -> new ListRowController());
+
     }
 
     private void openAddTaskForm() {
@@ -82,35 +74,4 @@ public class ListController {
         this.userId = userId;
     }
 
-    static class Cell extends ListCell<String>{
-        //Hbox is horizontal box
-        CheckBox checkBox = new CheckBox();
-        HBox hBox = new HBox();
-        Button button = new Button("Hello");
-        Label task = new Label();
-
-        Pane pane = new Pane();
-        Image icon = new Image("sample/assets/ic_addtasks.png");
-        ImageView iconImg = new ImageView(icon);
-
-
-
-        public Cell() {
-            super();
-
-            hBox.getChildren().setAll(checkBox, task, button);
-            hBox.setHgrow(pane, Priority.ALWAYS);
-        }
-
-        public void updateItem(String taskName, boolean empty){
-            super.updateItem(taskName, empty);
-            setText(null);
-            setGraphic(null);
-
-            if(taskName != null && !empty){
-                task.setText(taskName);
-                setGraphic(hBox);
-            }
-        }
-    }
 }
--- src/sample/controller/ListRowController.java
+++ src/sample/controller/ListRowController.java
@@ -1,4 +1,69 @@
 package sample.controller;
 
-public class ListRowController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import sample.models.Task;
+
+import java.io.IOException;
+
+public class ListRowController extends ListCell<Task> {
+
+    @FXML
+    private AnchorPane rootanchorpane_inlistrow;
+
+    @FXML
+    private ImageView taskicon_inlistrow;
+
+    @FXML
+    private Label tasknamelabel_inlistrow;
+
+    @FXML
+    private Label taskdescription_inlistrow;
+
+    @FXML
+    private Label datecreatedlabel_inlistrow;
+
+    @FXML
+    private ImageView deleteimage_inlistrow;
+
+    private FXMLLoader listRowFXMLloader;
+
+    @FXML
+    void initialize() {
+
+    }
+
+    @Override
+    protected void updateItem(Task myTask, boolean empty) {
+        super.updateItem(myTask, empty);
+        if (empty || myTask == null) {
+            setText(null);
+            setGraphic(null);
+        } else {
+            if(listRowFXMLloader == null){
+                listRowFXMLloader = new FXMLLoader(getClass().getResource("sample/view/listRow.fxml"));
+                listRowFXMLloader.setController(this);
+
+                try {
+                    listRowFXMLloader.setLocation(getClass().getResource("/sample/view/listRow.fxml"));
+                    listRowFXMLloader.load();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            tasknamelabel_inlistrow.setText(myTask.getTask());
+            taskdescription_inlistrow.setText(myTask.getDescription());
+            datecreatedlabel_inlistrow.setText(myTask.getDateCreated().toString());
+
+            setText(null);
+            setGraphic(rootanchorpane_inlistrow);
+
+        }
+    }
 }
